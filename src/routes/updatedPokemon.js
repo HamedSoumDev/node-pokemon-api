65d1234072913313deb9282945638a2ae9ec147a
HamedSoumDev <hamed.soumahoro.dev@gmail.com>
2025-06-25 13:50:52 +0000

    Sequelize validator && contraint.

--- src/routes/updatedPokemon.js
+++ src/routes/updatedPokemon.js
@@ -1,4 +1,4 @@
-const { where } = require("sequelize")
+const { ValidationError } = require("sequelize")
 const { Pokemon } = require("../db/sequelize")
 const pokemon = require("../models/pokemon")
 
@@ -9,10 +9,23 @@ module.exports = (app) => {
             where: {id: id}
         })
         .then(_ => {
-            Pokemon.findByPk(id).then(pokemon => {
+           return Pokemon.findByPk(id).then(pokemon => {
+                 if(pokemon === null) {
+                const message = `Le pokémon demandé n\'existe pas. Réessayer avec un autre identifiant`;
+                return res.status(404).json({message})
+            }
                 const message = `Le pokemon ${pokemon.name} a bien été modifié.`
                 res.json({message: message, data: pokemon})
             })
+        }) .catch(error => {
+            if(error instanceof ValidationError){
+                return res.status(400).json({message: error.message})
+            }
+              if(error instanceof UniqueConstraintError){
+                return res.status(400).json({message: error.message, data: error})
+            }
+            const message = `Le pokémon n\'a pas pu être modifié. Réessayez dans quelques instants.`
+            res.status(500).json({message, data: error})
         })
     })
 }
